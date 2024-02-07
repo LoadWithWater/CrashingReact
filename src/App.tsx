99b10b644eaa7eb899b9c1fa177c6a22aaf071a6
Kim <rlarhkdwo98@naver.com>
2024-02-08 00:21:50 +0900

    react-router-dom 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import './App.css';
+import Dom from "./routes/react_router_dom/index"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='mainpageTitle'>
+        <h1>React 연습용</h1>
+      </div>
+      <nav>
+        <Link to="/src/routes/react_router_dom">DOM</Link>
+      </nav>
+
+
+      <Routes>
+        <Route path='/src/routes/react_router_dom' element={<Dom/>}/>
+
+      </Routes>
+    </>
   );
 }
 
